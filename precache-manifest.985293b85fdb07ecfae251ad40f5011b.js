2fc74906281de5d0e205cd5b8ecadfdf09e60744
NomenNescio <u1201416@unimilitar.edu.co>
2020-01-20 16:34:51 -0500

    Updates

---
+++ precache-manifest.985293b85fdb07ecfae251ad40f5011b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe20e4ea701db35e4110bfa467445e13",
+    "url": "/recommend-a-movie-release/index.html"
+  },
+  {
+    "revision": "c3d4d9033fb0f692465f",
+    "url": "/recommend-a-movie-release/static/css/main.482b200b.chunk.css"
+  },
+  {
+    "revision": "d6e2cab4a21052c26ee8",
+    "url": "/recommend-a-movie-release/static/js/2.0ba41a17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recommend-a-movie-release/static/js/2.0ba41a17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3d4d9033fb0f692465f",
+    "url": "/recommend-a-movie-release/static/js/main.4656eb91.chunk.js"
+  },
+  {
+    "revision": "8575c277c086793796ce",
+    "url": "/recommend-a-movie-release/static/js/runtime-main.467a71dd.js"
+  },
+  {
+    "revision": "d53a55c81e7d4fd92eb717fab2f8767e",
+    "url": "/recommend-a-movie-release/static/media/not-found.d53a55c8.png"
+  }
+]);
